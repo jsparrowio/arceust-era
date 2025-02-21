b6736677f1698fca0d4d7e5f1732cb8c610d5d7f
Zachary LaCore <zlacore12@gmail.com>
2025-02-20 22:06:43 -0600

    ironed out undefined bug (thx chris)

--- client/src/pages/Cave.tsx
+++ client/src/pages/Cave.tsx
@@ -5,7 +5,6 @@ import '../assets/biome.css'
 export const Cave = () => {
     const pokeArr = ["Zubat", "Golbat", "Geodude", "Graveler", "Gastly", "Dunsparce", "Cubone", "Deino"]
     const itemArr = ["potion", "poke-ball"]
-    const randomItem = itemArr[Math.floor(Math.random() * itemArr.length)]
     const getPokemon = async () => {
         try {
             const randomPokemon = pokeArr[Math.floor(Math.random() * pokeArr.length)]
@@ -41,7 +40,7 @@ export const Cave = () => {
     const [isShiny, setShiny] = useState<boolean>(false)
     const [item, setItem] = useState<Record<string, any>>({})
     const [narration, setNarration] = useState<string>('')
-    const [num, setNum] = useState<number>()
+    // const [num, setNum] = useState<number>()
     const [clicked, setClicked] = useState<boolean>(false)
 
 
@@ -49,17 +48,17 @@ export const Cave = () => {
     }, [narration])
 
     // const shinyChance = () => {
-        // const shinyChance = Math.random()
-        // if (shinyChance > .80) {
-        //     shiny = true
-        // }
-        // return shiny
+    // const shinyChance = Math.random()
+    // if (shinyChance > .80) {
+    //     shiny = true
+    // }
+    // return shiny
     // }
     const roll = () => {
         setClicked(true)
         const chances = [1, 2, 3]
         const randomNum = chances[Math.floor(Math.random() * chances.length)]
-        setNum(randomNum)
+        // setNum(randomNum)
         console.log(randomNum)
         if (randomNum === 1) {
             setloading(true)
@@ -73,11 +72,11 @@ export const Cave = () => {
                 }
                 console.log(isShiny)
                 setNarration(`A wild ${pokemon.name} appeared!`)
-                    
+
             })
             setItem({})
         } else if (randomNum === 2) {
-            getItem().then((item) => { setItem(item); console.log(item);  setNarration(`You found a(n) ${(item.name)}`) })
+            getItem().then((item) => { setItem(item); console.log(item); setNarration(`You found a(n) ${(item.name)}`) })
             setPoke({})
             // setNarration(`You found a(n) ${item.name}`)
         } else if (randomNum === 3) {
@@ -92,6 +91,22 @@ export const Cave = () => {
         setloading(false)
 
     }
+    const catchPkmn = async () => {
+        const coinFlip = Math.random()
+        if (coinFlip >= .5) {
+            setNarration(`Congratulations! You caught the ${poke.name}!`)
+            setPoke({})
+        } else {
+            setNarration(`Get some better pokeballs dweeb`)
+        }
+        // TODO: Set up graphql mutations to handle catching and storing pokemon in database
+    }
+
+    const grabItem = () => {
+        setNarration(`You picked up the ${item.name}.`)
+        setItem({})
+        // TODO: Set up graphql mutations to handle adding items to inventory
+    }
     return (
         <div>
             {!clicked && <h1>You enter the cave.</h1>}
@@ -101,19 +116,28 @@ export const Cave = () => {
                 {!loading && poke && !isShiny && <img className='pokeimg' src={poke?.sprites?.front_default} alt={poke.name} />}
                 {!loading && poke && isShiny && <img className='pokeimg' src={poke?.sprites?.front_shiny} alt={poke.name} />}
                 {!loading && item && <img className='itemimg' src={item?.sprites?.default} alt={item.name} />}
-                <button onClick={() => {roll()
-                    // let newNarration = ""
-                    // if (num === 1) {
-                    //     newNarration = `A wild ${poke.name} appeared!`
-                    // } else if (num === 2) {
-                    //     newNarration = `You found a(n) ${item.name}`
-                    // } else if (num === 3) {
-                    //     newNarration = "Nothing appeared..."
-                    // }
-                    // setNarration(newNarration)
-                }
-                    
-                }>Get Random Pokemon or Item or Nothing</button>
+                <div className="btndiv">
+                    <button onClick={() => {
+                        roll()
+                        // let newNarration = ""
+                        // if (num === 1) {
+                        //     newNarration = `A wild ${poke.name} appeared!`
+                        // } else if (num === 2) {
+                        //     newNarration = `You found a(n) ${item.name}`
+                        // } else if (num === 3) {
+                        //     newNarration = "Nothing appeared..."
+                        // }
+                        // setNarration(newNarration)
+                    }
+                    }>Continue!</button>
+                    {clicked && poke.name && <button onClick={() => {
+                        catchPkmn()
+                    }}>Catch it!</button>}
+                    {clicked && item.name && <button onClick={() => {
+                        grabItem()
+                    }}
+                    >Pick up!</button>}
+                </div>
             </div>
         </div>
     )
