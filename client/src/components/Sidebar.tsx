999673bfc13c150d86857bb7917c4efbb8903dad
krana <kranat10@gmail.com>
2025-03-03 01:35:19 -0600

    stashing changes

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -27,8 +27,12 @@ function Sidebar() {
         left: "15px",
         height: "calc(100vh -30px)",
         width: closed ? "80px" : "175px",
-        borderRadius: "10px",
         transition: "width 0.3s ease-in-out",
+        backgroundColor: "#682222",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        overflow: "hidden",
       }}
     >
       {/* Menu Icon for Collapsing the Sidebar */}
@@ -39,71 +43,72 @@ function Sidebar() {
           color: "#af4848",
           backgroundColor: "#682222",
           cursor: "pointer",
-          marginBottom: "0",
+          padding: "15px",
+          width: "100%",
           display: "block",
           textAlign: "center",
         }}
       />
       {/* Sidebar Main, hide when collapsed */}
       {!closed && (
-      <Menu
-        mode="vertical"
-        className="custom-menu"
-        inlineCollapsed={closed}
-        onClick={({ key }) => {
-          if (key === "signout") {
-            // TODO: sign out feature here
-          } else {
-            nav(key);
-          }
-        }}
-        style={{
-          width: closed ? "80px" : "175px",
-          textAlign: "left",
-          height: "100%",
-          borderRight: "none",
-          backgroundColor: "transparent",
-        }}
-        items={[
-          { label: "Home", key: "/", icon: <HomeOutlined /> },
-          {
-            label: "Safari Zone",
-            key: "safari-zone",
-            icon: <CompassOutlined />,
-            children: [
-              {
-                label: "Cave",
-                key: "/safari-zone/cave",
-                icon: <FilterOutlined />,
-              },
-              {
-                label: "Beach",
-                key: "/safari-zone/beach",
-                icon: <SunOutlined />,
-              },
-              {
-                label: "Grass",
-                key: "/safari-zone/grass",
-                icon: <WechatOutlined />,
-              },
-            ],
-          },
-          { label: "Party", key: "/party", icon: <TeamOutlined /> },
-          {
-            label: "Pokecenter",
-            key: "/pokecenter",
-            icon: <MedicineBoxOutlined />,
-          },
-          { label: "Profile", key: "/profile", icon: <UserOutlined /> },
-          {
-            label: "Signout",
-            key: "signout",
-            icon: <PoweroffOutlined />,
-            danger: true,
-          },
-        ]}
-      ></Menu>
-    )}
+        <Menu
+          mode="vertical"
+          className="custom-menu"
+          inlineCollapsed={closed}
+          onClick={({ key }) => {
+            if (key === "signout") {
+              // TODO: sign out feature here
+            } else {
+              nav(key);
+            }
+          }}
+          style={{
+            width: "100%",
+            textAlign: "left",
+            height: "100%",
+            borderRight: "none",
+            backgroundColor: "transparent",
+          }}
+          items={[
+            { label: "Home", key: "/", icon: <HomeOutlined /> },
+            {
+              label: "Safari Zone",
+              key: "safari-zone",
+              icon: <CompassOutlined />,
+              children: [
+                {
+                  label: "Cave",
+                  key: "/safari-zone/cave",
+                  icon: <FilterOutlined />,
+                },
+                {
+                  label: "Beach",
+                  key: "/safari-zone/beach",
+                  icon: <SunOutlined />,
+                },
+                {
+                  label: "Grass",
+                  key: "/safari-zone/grass",
+                  icon: <WechatOutlined />,
+                },
+              ],
+            },
+            { label: "Party", key: "/party", icon: <TeamOutlined /> },
+            {
+              label: "Pokecenter",
+              key: "/pokecenter",
+              icon: <MedicineBoxOutlined />,
+            },
+            { label: "Profile", key: "/profile", icon: <UserOutlined /> },
+            {
+              label: "Signout",
+              key: "signout",
+              icon: <PoweroffOutlined />,
+              danger: true,
+            },
+          ]}
+        ></Menu>
+      )}
     </div>
   );
 }
